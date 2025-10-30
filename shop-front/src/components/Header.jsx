8af972655dfeec75954fdf04387b76ebd3ae7e54
choieh <bobomi66@naver.com>
2025-10-30 11:50:04 +0900

    폴더 및 파일 셋팅

--- shop-front/src/components/Header.jsx
+++ shop-front/src/components/Header.jsx
@@ -1,14 +1,14 @@
 export default function Header() {
     return (
         <header role="banner">
-        <h1>Shop</h1>
-        <nav aria-label="주요 메뉴">
-            <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-            </ul>
-        </nav>
+            <h1>Shop</h1>
+            <nav aria-label="주요 메뉴">
+                <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
         </header>
     );
 }
